3fa91eb166e78073403933886058d33ea712cac7
Pratyush kumar singh <23BDA70101@cuchd.in>
2026-02-26 11:25:17 +0530

    Added backend experiment-8 and experiment-9

---
+++ backend/experiment-8/run.py
@@ -0,0 +1,8 @@
+from app import app
+
+@app.route('/')
+def home():
+    return "App running"
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
